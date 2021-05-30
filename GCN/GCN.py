e3ede10d9de0b3849d0a5180a92233c0b42375a9
Alfred Feldmeyer <6654064+byteSamurai@users.noreply.github.com>
2021-05-30 18:55:34 +0200

    Fix diagonal degree matrix creation (#14)

--- GCN/GCN.py
+++ GCN/GCN.py
@@ -22,7 +22,7 @@ class GCNConv(nn.Module):
     def __init__(self, A, in_channels, out_channels):
         super(GCNConv, self).__init__()
         self.A_hat = A+torch.eye(A.size(0))
-        self.D     = torch.diag(torch.sum(A,1))
+        self.D     = torch.diag(torch.sum(self.A_hat,1))
         self.D     = self.D.inverse().sqrt()
         self.A_hat = torch.mm(torch.mm(self.D, self.A_hat), self.D)
         self.W     = nn.Parameter(torch.rand(in_channels,out_channels, requires_grad=True))
